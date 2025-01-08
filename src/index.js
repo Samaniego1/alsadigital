b44fda24ccfb3baeba79d7da3b0b0ac24adda906
Samaniego1 <samaniegodomingo@gmail.com>
2025-01-07 22:17:35 -0300

    Primer commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const App = () => <h1>Hello, Alsa Digital!</h1>;
+const App = () => <h1>Oi</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
